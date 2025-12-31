a6babe69057f4e57ebcd242d58a94951b94e26e4
alr11118 <alara.engin2008@gmail.com>
2025-12-31 10:47:14 -0800

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,31 +1,24 @@
 import numpy as np
-sudokuSize = 3
+sudokuSize = 4
 def print_board(board):
-    for i in range(sudokuSize*2):
+    for i in range(sudokuSize):
         print(board[i])
 
 def count_empty_cells(board):
     return np.sum(board == 0)
 
-"""""Example usage:
-board = [[5, 3, 0, 0, 7, 0, 0, 0],
-         [6, 0, 0, 1, 9, 5, 0, 0],
-         [0, 9, 8, 0, 0, 0, 0, 0],
-         [8, 0, 0, 0, 6, 0, 0, 3],
-         [4, 0, 0, 8, 0, 3, 0, 1],
-         [7, 0, 0, 0, 2, 0, 0, 6],
-         [0, 6, 0, 0, 0, 0, 2, 0],
-         [0, 0, 0, 4, 1, 9, 0, 5]]
-"""
-board = np.array([[1, 2, 0, 0, 3, 2],
-                  [1, 2, 0, 0, 3, 2],
-                  [1, 2, 0, 0, 3, 2],
-                  [1, 2, 0, 0, 3, 2],
-                  [1, 2, 0, 0, 3, 2],
-                  [1, 2, 0, 0, 3, 2]])
-print_board(board)
+def checkRow (board, rowNum):
+    for i in range (sudokuSize):
+        print("")
+
+
+
+board = np.array([[1, 2, 3, 2],
+                  [1, 4, 3, 2],
+                  [1, 2, 3, 2],
+                  [1, 2, 3, 2]])
 empty_cells = count_empty_cells(board)
 
-
+print_board(board)
 print(f"Number of empty cells: {empty_cells}")
 print(f"number of possible combinations {sudokuSize**empty_cells}")
