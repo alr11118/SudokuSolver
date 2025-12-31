db911a3e10d595ef0717989d01b001c2c088bd52
alr11118 <alara.engin2008@gmail.com>
2025-12-31 10:54:29 -0800

    edited sudokuSolver

--- sudoku.py
+++ sudoku.py
@@ -1,5 +1,6 @@
 import numpy as np
 sudokuSize = 4
+neededTotal = 4+3+2+1
 def print_board(board):
     for i in range(sudokuSize):
         print(board[i])
@@ -7,9 +8,28 @@ def print_board(board):
 def count_empty_cells(board):
     return np.sum(board == 0)
 
+# Find a better way to check them (doesnt work on 3 wrong places)
 def checkRow (board, rowNum):
+    total = 0
     for i in range (sudokuSize):
-        print("")
+        total += board[rowNum][i]
+    if neededTotal == total:
+        print("Good row")
+        return True
+    else:
+        print("Bad row")
+        return False
+    
+def checkColumn (board, columnNum):
+    total = 0
+    for i in range (sudokuSize):
+        total += board[i][columnNum]
+    if neededTotal == total:
+        print("Good column")
+        return True
+    else:
+        print("Bad column")
+        return False
 
 
 
@@ -19,6 +39,9 @@ board = np.array([[1, 2, 3, 2],
                   [1, 2, 3, 2]])
 empty_cells = count_empty_cells(board)
 
+
+checkRow(board, 0)
+checkColumn(board, 1)
 print_board(board)
 print(f"Number of empty cells: {empty_cells}")
 print(f"number of possible combinations {sudokuSize**empty_cells}")
