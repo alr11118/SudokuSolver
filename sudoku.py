d012a9ed2d39a0d2cbab694665d7c58f9fac91ae
alr11118 <alara.engin2008@gmail.com>
2025-12-31 11:09:45 -0800

    fixed functions more...

--- sudoku.py
+++ sudoku.py
@@ -12,7 +12,7 @@ def count_empty_cells(board):
 def checkRow (board, rowNum):
     row = board[rowNum]
     for i in range (len(row)):
-        for j in range (len(row)):
+        for j in range (i+1, len(row)):
             if row[j] == row[i]:
                 print("Bad row")
                 return False
@@ -22,24 +22,23 @@ def checkRow (board, rowNum):
 def checkColumn (board, ColumnNum):
     column = board[ColumnNum]
     for i in range (len(column)):
-        for j in range (len(column)):
+        for j in range (i+1, len(column)):
             if column[j] == column[i]:
                 print("Bad column")
                 return False
     print("Good column")
     return True 
 
-
-
-board = np.array([[2, 2, 4, 2],
-                  [1, 4, 3, 2],
-                  [1, 2, 3, 2],
-                  [1, 2, 3, 2]])
+board = np.array([[1, 4, 3, 2],
+                  [3, 2, 1, 4],
+                  [4, 1, 2, 3],
+                  [2, 3, 4, 1]])
 empty_cells = count_empty_cells(board)
 
 
 checkRow(board, 0)
 checkColumn(board, 1)
+checkColumn(board, 3)
 print_board(board)
 print(f"Number of empty cells: {empty_cells}")
 print(f"number of possible combinations {sudokuSize**empty_cells}")
