7955bb9fb5be3aab39f022c109aa3268cfc33db6
alr11118 <alara.engin2008@gmail.com>
2025-12-31 11:19:16 -0800

    added cehckboard

--- sudoku.py
+++ sudoku.py
@@ -14,9 +14,9 @@ def checkRow (board, rowNum):
     for i in range (len(row)):
         for j in range (i+1, len(row)):
             if row[j] == row[i]:
-                print("Bad row")
+                #print("Bad row")
                 return False
-    print("Good row")
+    #print("Good row")
     return True 
 # Checks if there is any repeated numbers in COLUMNS
 def checkColumn (board, ColumnNum):
@@ -24,21 +24,33 @@ def checkColumn (board, ColumnNum):
     for i in range (len(column)):
         for j in range (i+1, len(column)):
             if column[j] == column[i]:
-                print("Bad column")
+                #print("Bad column")
                 return False
-    print("Good column")
+    #print("Good column")
     return True 
+
+def checkBoard (board):
+    for i in range(sudokuSize):
+        if (checkRow(board, i) != True):
+            print("Board is bad")
+            return False
+        if (checkColumn(board, i) != True):
+            print("Board is bad")
+            return False
+    print("Board is good")
+    return True
 
 board = np.array([[1, 4, 3, 2],
                   [3, 2, 1, 4],
                   [4, 1, 2, 3],
                   [2, 3, 4, 1]])
+board2 = np.array([[1, 4, 3, 2],
+                   [3, 4, 1, 4],
+                   [4, 1, 2, 3],
+                   [2, 3, 4, 1]])
 empty_cells = count_empty_cells(board)
 
-
-checkRow(board, 0)
-checkColumn(board, 1)
-checkColumn(board, 3)
-print_board(board)
+checkBoard(board)
+checkBoard(board2)
 print(f"Number of empty cells: {empty_cells}")
 print(f"number of possible combinations {sudokuSize**empty_cells}")
