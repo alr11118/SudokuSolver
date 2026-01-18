a95682de07a7db0930931883e55f45e311c93d86
alr11118 <alara.engin2008@gmail.com>
2026-01-17 18:25:08 -0800

    made the board 2D array

--- solver.py
+++ solver.py
@@ -3,3 +3,15 @@
 # Find one that works
 # Go to the next square and repeat
 # If cannot solve backtrack (go back a sept and do not put the same number)
+
+board = [
+    [7, 8, 0, 4, 0, 0, 1, 2, 0],
+    [6, 0, 0, 0, 7, 5, 0, 0, 9],
+    [0, 0, 0, 6, 0, 1, 0, 7, 8],
+    [0, 0, 7, 0, 4, 0, 2, 6, 0],
+    [0, 0, 1, 0, 5, 0, 9, 3, 0],
+    [9, 0, 4, 0, 6, 0, 0, 0, 5],
+    [0, 7, 0, 3, 0, 0, 0, 1, 2],
+    [1, 2, 0, 0, 0, 7, 4, 0, 0],
+    [0, 4, 9, 2, 0, 6, 0, 0, 7]
+]
