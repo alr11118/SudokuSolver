957e0c5a41cf9050c8e4c6857c310802611437c9
alr11118 <alara.engin2008@gmail.com>
2026-01-17 18:36:13 -0800

    printBoard Function

--- solver.py
+++ solver.py
@@ -15,3 +15,20 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+def printBoard(board):
+
+    for i in range (len(board)):
+        if i % 3 == 0 and i != 0:
+            print("------------------------")
+
+        for j in range (len(board[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(board[i][j])
+            else:
+                print(str(board[i][j]) + " ", end="")
+
+printBoard(board)
