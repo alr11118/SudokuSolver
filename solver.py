3b36fed73685e2709ea3d5fc9e6d2db597d55b6c
alr11118 <alara.engin2008@gmail.com>
2026-01-17 18:15:43 -0800

    presudo code

--- solver.py
+++ solver.py
@@ -2,4 +2,4 @@
 # Try all numbers
 # Find one that works
 # Go to the next square and repeat
-# If cannot solve backtrack
+# If cannot solve backtrack (go back a sept and do not put the same number)
